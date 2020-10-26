98035b386fc3f6379c38fdae574b419bace5bf93
Jubin Jang <sunshine0429@snu.ac.kr>
2020-10-26 22:03:44 +0900

    Delete all things.

--- resources/views/components/upcoming-events.blade.php
+++
@@ -1,7 +0,0 @@
-## Upcoming Events
-
-Event | When | Duration
-----|----|----
-@foreach($events as $event)
-{{ $redactName ? 'Redacted' : $event->name }} | {{ $event->startDate->diffForHumans() }} | {{ $event->startDate->diffInHours($event->endDate) }} {{ \Illuminate\Support\Str::plural('hour', $event->startDate->diffInHours($event->endDate)) }}
-@endforeach
--- resources/views/profile.blade.php
+++
@@ -1,5 +0,0 @@
-# My GitHub profile
-
-<x-currently-playing-on-spotify />
-
-<x-upcoming-events :redact-name="true" />
